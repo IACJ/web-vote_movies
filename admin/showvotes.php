99c4a26b09ea5b5cbf33812a5b96461d61f131aa
ACJ <178480641@qq.com>
2016-10-15 00:34:40 +0800

    it should be ok

---
+++ admin/showvotes.php
@@ -0,0 +1,31 @@
+<?php 
+session_start();
+if (!isset($_SESSION['token']) || $_SESSION['token'] !== "zhegemimahaonancaiyahaonancaiacjacjacjacj"){
+		header("Location: ./login.php"); 
+	exit;
+} 
+
+header('Content-Type: text/html; charset=utf-8');
+require_once "../config.php";
+
+
+$link  = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PWD);
+$link->exec('SET NAMES '.DB_CHARSET);
+		
+$sql = "SELECT `id`, `name`,`vote` FROM `vote` ";
+$stmt = $link->prepare($sql);
+$stmt->execute();
+$all = $stmt->fetchALL(PDO::FETCH_ASSOC);
+echo"<pre>";
+print_r($all);
+echo"</pre>";
+
+
+
+
+
+
+
+
+
+
